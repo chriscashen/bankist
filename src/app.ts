190a93f55f4b58eae292e0d3b9315b413461b942
chriscashen <chris.cashen@hotmail.com>
2023-05-29 18:24:44 -0700

    add login function

--- src/app.ts
+++ src/app.ts
@@ -40,25 +40,25 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value') as HTMLElement;
-const labelSumIn = document.querySelector('.summary__value--in') as HTMLElement;
-const labelSumOut = document.querySelector('.summary__value--out') as HTMLElement;
-const labelSumInterest = document.querySelector('.summary__value--interest') as HTMLElement;
+const labelWelcome = <HTMLElement>document.querySelector('.welcome');
+const labelDate = <HTMLElement>document.querySelector('.date');
+const labelBalance = <HTMLElement>document.querySelector('.balance__value');
+const labelSumIn = <HTMLElement>document.querySelector('.summary__value--in');
+const labelSumOut = <HTMLElement>document.querySelector('.summary__value--out');
+const labelSumInterest = <HTMLElement>document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements') as HTMLElement;
+const containerApp = <HTMLElement>document.querySelector('.app');
+const containerMovements = <HTMLElement>document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
+const btnLogin = <HTMLButtonElement>document.querySelector('.login__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
+const inputLoginUsername = <HTMLInputElement>document.querySelector('.login__input--user');
+const inputLoginPin = <HTMLInputElement>document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
@@ -80,7 +80,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 
-// DISPAY TRANSATIONS 
+/////////////////////////////////////////////////
+// DISPAY FUNCTIONS
+/////////////////////////////////////////////////
 const displayTranactions = (account: Account) => {
   // CLEAR CONTAINER
   containerMovements.innerHTML = '';
@@ -104,6 +106,56 @@ const displayBalance = function (account: Account): void {
   labelBalance.textContent = `${accountBalance} EUR`
 }
 
+// DISPLAY DEPOSIT SUMMARY
+const displayDeposits = (account: Account): void => {
+  const total = filterMovement(account, MovementFilter.DEPOSIT)?.reduce((acc, cur) => acc + cur, 0)
+
+  labelSumIn.textContent = `${total}€`
+}
+
+// DISPLAY WITHDRAWL SUMMARY
+const displayWidthdrawls = (account: Account): void => {
+  const total = filterMovement(account, MovementFilter.WITHDRAWAL)?.reduce((acc, cur) => acc + cur, 0)
+
+  labelSumOut.textContent = `${total}€`
+}
+
+// DISPLAY INTEREST SUMMARY
+const displayInterestSummary = (account: Account): void => {
+  const deposits = filterMovement(account, MovementFilter.DEPOSIT)
+  // TOTAL PAYMENTS
+  const total = deposits?.map(deposit => deposit * 0.012).reduce((acc, int) => acc + int, 0)
+  //  DISPLAY SUM
+  labelSumInterest.textContent = `${total}€`
+}
+
+/////////////////////////////////////////////////
+// RETURN FUNCTIONS
+/////////////////////////////////////////////////
+
+// RETURN ACCOUNT BALANCE
+const accountBalance = (account: Account): number => {
+  return account.movements.reduce((acc, cur) => acc + cur, 0)
+}
+
+// CREATE MOVEMENT TILE DATA
+const movementsDisplay = (movements: number[]): string[] => {
+  const movementTiles = movements.map((mov, i) => {
+    return `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${mov}`
+  })
+  return movementTiles
+}
+
+// MAX DEPOSIT
+const maxDeposit = (account: Account): number => {
+  const movements = account.movements
+  return movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0])
+}
+
+/////////////////////////////////////////////////
+// CONVERSION FUNCTIONS
+/////////////////////////////////////////////////
+
 // CONVERT CURRENCY FUNCTION
 const convertCurrancy = (amount: number, to: Currency, from: Currency): number => {
   const fromRate = rates[from];
@@ -120,21 +172,10 @@ const movementsUSD = movements.map((movement) => {
   return convertCurrancy(movement, Currency.USD, Currency.EUR)
 })
 
-// CREATE MOVEMENT TILE DATA
-const movementsDisplay = (movements: number[]): string[] => {
-  const movementTiles = movements.map((mov, i) => {
-    return `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${mov}`
-  })
-  return movementTiles
-}
 
-
-// GERERATE USERNAMES
-const createUserName = (accounts: Account[]): void => {
-  accounts.forEach(account => {
-    account.userName = account.owner.toLowerCase().split(' ').reduce((userName, name) => userName + name[0], '')
-  });
-}
+/////////////////////////////////////////////////
+// UTILITY FUNCTIONS
+/////////////////////////////////////////////////
 
 // FILTER MOVEMENTS
 const filterMovement = (account: Account, filter: MovementFilter): number[] | undefined => {
@@ -147,49 +188,10 @@ const filterMovement = (account: Account, filter: MovementFilter): number[] | un
       console.log('Invalid transaction type');
   }
 }
-const deposits = filterMovement(account1, MovementFilter.DEPOSIT)
-const withdrawals = filterMovement(account2, MovementFilter.WITHDRAWAL)
 
-// ACCOUNT BALANCE
-const accountBalance = (account: Account): number => {
-  return account.movements.reduce((acc, cur) => acc + cur, 0)
-}
-
-
-// MAX DEPOSIT
-const maxDeposit = (account: Account): number => {
-  const movements = account.movements
-  return movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0])
-}
-
-
-// DISPLAY DEPOSIT SUMMARY
-const displayDeposits = (account: Account): void => {
-  const total = filterMovement(account, MovementFilter.DEPOSIT)?.reduce((acc, cur) => acc + cur, 0)
-
-  labelSumIn.textContent = `${total}€`
-}
-
-// DISPLAY WITHDRAWL SUMMARY
-const displayWidthdrawl = (account: Account): void => {
-  const total = filterMovement(account, MovementFilter.WITHDRAWAL)?.reduce((acc, cur) => acc + cur, 0)
-
-  labelSumOut.textContent = `${total}€`
-}
-
-// DISPLAY INTEREST SUMMARY
-const displayInterestSummary = (account: Account): void => {
-  const deposits = filterMovement(account, MovementFilter.DEPOSIT)  
-  // TOTAL PAYMENTS
-  const total =  deposits?.map(deposit => deposit * 0.012).reduce((acc, int) => acc + int, 0)
-  //  DISPLAY SUM
-  labelSumInterest.textContent = `${total}€`
-}
-
-displayDeposits(account1)
-displayWidthdrawl(account1)
-displayBalance(account1)
-displayInterestSummary(account1)
+/////////////////////////////////////////////////
+// ACCOUNT FUNCTIONS
+/////////////////////////////////////////////////
 
 
 // FIND ACCOUNT BY NAME
@@ -199,10 +201,75 @@ const findAccountBy = (accounts: Account[], by: AccountProperty, searchTerm: str
   return account
 }
 
-const findAccountByName = (accounts: Account[] , owner: string): Account | undefined => {
+const findAccountByName = (accounts: Account[], owner: string): Account | undefined => {
   const account = accounts.find(acc => acc.owner === owner)
   return account
 }
 
 
-console.log(findAccountBy(accounts, AccountProperty.Owner, `Sarah Smith`))
+/////////////////////////////////////////////////
+/// LOGIN IN FUNCTION
+/////////////////////////////////////////////////
+
+const handleLoginSuccess = (account: Account): void => {
+  //DISPLAY UI AND MESSAGE
+  labelWelcome.textContent = `Welcome back, ${account.owner.split(" ")[0]}`
+  containerApp.style.opacity = '100';
+  //DISPLAY MOVEMENTS
+  displayTranactions(account)
+
+  //DISPAY BALANCE
+  displayBalance(account)
+
+  //DISPLAY SUMMARY
+  displayDeposits(account)
+  displayWidthdrawls(account)
+  displayInterestSummary(account)
+}
+
+
+/// LOGIN IN BUTTON HANDLER
+btnLogin.addEventListener('click', (e: Event) => {
+  e.preventDefault()
+  const userName = inputLoginUsername.value
+  const pin = inputLoginPin.value
+  if (!userName) {
+    alert('Please enter your username');
+    return;
+  }
+
+  if (!pin) {
+    alert('please enter your pin');
+    return
+  }
+
+  const accountLookup = findAccountBy(accounts, AccountProperty.UserName, userName)
+  if (+pin === accountLookup?.pin) {
+    currentAccount = accountLookup
+    handleLoginSuccess(currentAccount)
+  } else {
+    alert("Forgot your username or password?, click need help")
+    return;
+  }
+
+})
+
+/////////////////////////////////////////////////
+// APP INIT FUNCTIONS
+/////////////////////////////////////////////////
+
+// GERERATE USERNAMES
+const createUserNames = (accounts: Account[]): void => {
+  accounts.forEach(account => {
+    account.userName = account.owner.toLowerCase().split(' ').reduce((userName, name) => userName + name[0], '')
+  });
+}
+
+
+/////////////////////////////////////////////////
+/// START APPLICATION 
+/////////////////////////////////////////////////
+// CURRENT ACCOUNT
+let currentAccount: Account | undefined;
+// ASSIGN USERNAMES TO ACCOUNTS
+createUserNames(accounts);
